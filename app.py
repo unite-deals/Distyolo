343fbf02b2a0c030faebe90d402082cd33a9ee80
unite-deals <80326413+unite-deals@users.noreply.github.com>
2023-06-01 14:04:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)  # Use 0 for webcam, or provide the path to a video file
+
+def load_yolo():
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    return net, classes, output_layers
+
+def detect_objects(frame, net, output_layers):
+    height, width, channels = frame.shape
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    return class_ids, confidences, boxes
+
+def measure_distance(frame, known_width, focal_length, box):
+    object_width = box[2]
+    distance = (known_width * focal_length) / object_width
+    return distance
+
+def generate_frames():
+    net, classes, output_layers = load_yolo()
+    known_width = 20  # Width of the object in cm
+    focal_length = 500  # Focal length in pixels
+
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+
+        class_ids, confidences, boxes = detect_objects(frame, net, output_layers)
+
+        for i, box in enumerate(boxes):
+            distance = measure_distance(frame, known_width, focal_length, box)
+            label = f"{classes[class_ids[i]]} ({distance:.2f} cm)"
+            x, y, w, h = box
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
